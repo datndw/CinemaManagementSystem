f0d2b3945fa9c9d6db12fdd1d0bf30056ea6df27
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-07-14 12:42:11 +0700

    datnd add company request

---
+++ CinemaManagementSystem.Application/Features/Companies/Handlers/Queries/GetCompaniesRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CinemaManagementSystem.Application.Contracts.Persistence;
+using CinemaManagementSystem.Application.DTOs.Company;
+using CinemaManagementSystem.Application.Features.Companies.Requests.Queries;
+using MediatR;
+
+namespace CinemaManagementSystem.Application.Features.Companies.Handlers.Queries
+{
+    public class GetCompaniesRequestHandler : IRequestHandler<GetCompaniesRequest, List<CompanyDTO>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetCompaniesRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<CompanyDTO>> Handle(GetCompaniesRequest request, CancellationToken cancellationToken)
+        {
+            var actors = await _unitOfWork.CompanyRepository.GetAllAsync();
+            return _mapper.Map<List<CompanyDTO>>(actors);
+        }
+    }
+}
+
---
+++ CinemaManagementSystem.Application/Features/Companies/Handlers/Queries/GetCompanyRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CinemaManagementSystem.Application.Contracts.Persistence;
+using CinemaManagementSystem.Application.DTOs.Company;
+using CinemaManagementSystem.Application.Exceptions;
+using CinemaManagementSystem.Application.Features.Companies.Requests.Queries;
+using CinemaManagementSystem.Domain.Entities;
+using MediatR;
+
+namespace CinemaManagementSystem.Application.Features.Companies.Handlers.Queries
+{
+    public class GetCompanyRequestHandler : IRequestHandler<GetCompanyRequest, CompanyDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetCompanyRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<CompanyDTO> Handle(GetCompanyRequest request, CancellationToken cancellationToken)
+        {
+            var company = await _unitOfWork.CompanyRepository.GetAsync(request.Id);
+            if (company == null)
+            {
+                throw new NotFoundException(nameof(Company), request.Id);
+            }
+            return _mapper.Map<CompanyDTO>(company);
+        }
+    }
+}
