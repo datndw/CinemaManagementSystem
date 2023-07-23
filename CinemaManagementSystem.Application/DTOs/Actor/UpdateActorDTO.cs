97ecc60abb995f57ff899f9f85c2c4259c4e6333
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-07-23 12:07:39 +0700

    datnd update actor api command

--- CinemaManagementSystem.Application/DTOs/Actor/UpdateActorDTO.cs
+++ CinemaManagementSystem.Application/DTOs/Actor/UpdateActorDTO.cs
@@ -7,7 +7,6 @@ namespace CinemaManagementSystem.Application.DTOs.Actor
 	{
         public string Name { get; set; }
         public string Description { get; set; }
-        public string? ImageUrl { get; set; }
         public DateTime? BirthDate { get; set; }
         public string Gender { get; set; }
     }
