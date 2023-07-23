cb8aed74fd152b02f7a0039b5b9f347f7e901ffd
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-07-23 12:37:33 +0700

    datnd remove some confilct props

--- CinemaManagementSystem.Application/DTOs/User/CreateUserDTO.cs
+++ CinemaManagementSystem.Application/DTOs/User/CreateUserDTO.cs
@@ -8,7 +8,6 @@ namespace CinemaManagementSystem.Application.DTOs.User
         public string Firstname { get; set; }
         public string? MiddleName { get; set; }
         public string Lastname { get; set; }
-        public string? ImageUrl { get; set; }
         public Guid? CompanyId { get; set; }
         public DateTime? BirthDate { get; set; }
     }
